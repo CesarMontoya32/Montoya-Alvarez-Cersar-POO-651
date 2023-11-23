31e8a1d9dc7619ebb4159a9f3997eaec34fe70fb
Cesar Montoya <cesar.09azpilicueta@gmail.com>
2023-11-23 15:16:04 -0600

    Tercera Unidad

---
+++ Unidad3/pkg651puntodeventa/Ventas.java
@@ -0,0 +1,51 @@
+package pkg651puntodeventa;
+
+
+public class Ventas {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+   private int id;
+   private String descripcion;
+   private double precio;
+   private int cantidad;
+   private double importe;
+   
+}
